5b5606d03198016b79eaefef3510cfabc7e9fa77
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-07-24 17:57:51 +0700

    Added perPage menu

--- src/common/components/Pagination/PageMenu.js
+++ src/common/components/Pagination/PageMenu.js
@@ -2,47 +2,68 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Icon, Menu } from 'semantic-ui-react';
 import get from 'lodash.get';
-import { setPage } from './actions';
+import { setPage, setPerPage } from './actions';
 
 class PageMenu extends React.Component {
+  firstPage = 1;
+
   createPageChangeHandler = toPage => () => {
     const { setPageDispatcher, onPageChange, perPage, search, sort, filters, lastPage } = this.props;
-    if (toPage >= 1 && toPage <= lastPage) {
+    if (toPage >= this.firstPage && toPage <= lastPage) {
       setPageDispatcher(toPage);
-      return onPageChange({page: toPage, perPage, search, sort, filters, lastPage});
+      return onPageChange({ page: toPage, perPage, search, sort, filters, lastPage});
+    }
+    return null;
+  };
+
+  createPerPageChangeHandler = perPage => () => {
+    const { setPageDispatcher, setPerPageDispatcher, onPageChange, search, sort, filters, lastPage } = this.props;
+    if (perPage > 0) {
+      setPageDispatcher(this.firstPage);
+      setPerPageDispatcher(perPage);
+      return onPageChange({ page: this.firstPage, perPage, search, sort, filters, lastPage});
     }
     return null;
   };
 
   render() {
-    const firstPage = 1;
-    const { size, floated, page, lastPage, displayRange } = this.props;
+    const { size, floated, page, perPage, lastPage, displayRange, showPerPageMenu } = this.props;
 
     let menuItemIndexes = [];
-    let lower = Math.max(firstPage, page - displayRange);
+    let lower = Math.max(this.firstPage, page - displayRange);
     let upper = Math.min(lastPage, page + displayRange);
     for (let i = lower; i <= upper; i++) {
       menuItemIndexes.push(i);
     }
 
     return (
-      <Menu pagination size={size} floated={floated}>
-        <Menu.Item link icon disabled={page <= firstPage} onClick={this.createPageChangeHandler(page-1)}>
-          <Icon name='left chevron' />
-        </Menu.Item>
-        {lower > firstPage &&
-          <Menu.Item disabled>...</Menu.Item>
+      <div>
+        {showPerPageMenu &&
+        <Menu size={size} floated={floated}>
+          {[10, 25, 50, 100].map(i => (
+            <Menu.Item link active={perPage === i} key={i} onClick={this.createPerPageChangeHandler(i)}>{i}</Menu.Item>
+          ))}
+        </Menu>
         }
-        {menuItemIndexes.map(i => (
-          <Menu.Item link active={page === i} key={i} onClick={this.createPageChangeHandler(i)}>{i}</Menu.Item>
-        ))}
-        {upper < lastPage &&
-          <Menu.Item disabled>...</Menu.Item>
-        }
-        <Menu.Item link icon disabled={page >= lastPage} onClick={this.createPageChangeHandler(page+1)}>
-          <Icon name='right chevron' />
-        </Menu.Item>
-      </Menu>
+
+        <Menu pagination size={size} floated={floated}>
+          <Menu.Item link icon disabled={page <= this.firstPage} onClick={this.createPageChangeHandler(page-1)}>
+            <Icon name='left chevron' />
+          </Menu.Item>
+          {lower > this.firstPage &&
+            <Menu.Item disabled>...</Menu.Item>
+          }
+          {menuItemIndexes.map(i => (
+            <Menu.Item link active={page === i} key={i} onClick={this.createPageChangeHandler(i)}>{i}</Menu.Item>
+          ))}
+          {upper < lastPage &&
+            <Menu.Item disabled>...</Menu.Item>
+          }
+          <Menu.Item link icon disabled={page >= lastPage} onClick={this.createPageChangeHandler(page+1)}>
+            <Icon name='right chevron' />
+          </Menu.Item>
+        </Menu>
+      </div>
     );
   }
 }
@@ -50,13 +71,15 @@ class PageMenu extends React.Component {
 PageMenu.defaultProps = {
   displayRange: 2,
   storeKey: '',
-  onPageChange: () => {}
+  onPageChange: () => {},
+  showPerPageMenu: true,
+  defaultPerPage: 25
 };
 
 const mapStateToProps = (state, ownProps) => {
   return {
     page: get(state, ownProps.storeKey + '.page', 1),
-    perPage: get(state, ownProps.storeKey + '.perPage'),
+    perPage: get(state, ownProps.storeKey + '.perPage', ownProps.defaultPerPage),
     search: get(state, ownProps.storeKey + '.search'),
     sort: get(state, ownProps.storeKey + '.sort'),
     filters: get(state, ownProps.storeKey + '.filters'),
@@ -66,7 +89,8 @@ const mapStateToProps = (state, ownProps) => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
-    setPageDispatcher: toPage => dispatch(setPage(ownProps.storeKey, toPage))
+    setPageDispatcher: toPage => dispatch(setPage(ownProps.storeKey, toPage)),
+    setPerPageDispatcher: perPage => dispatch(setPerPage(ownProps.storeKey, perPage))
   };
 };
 
