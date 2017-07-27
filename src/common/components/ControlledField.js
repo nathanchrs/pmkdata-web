371fa24ee398d5e5622cfe092fbca669fac08261
Jonathan Christopher <jonathan.christopher@outlook.com>
2017-07-27 10:42:45 +0700

    Added Dropdown support to ControlledField

--- src/common/components/ControlledField.js
+++ src/common/components/ControlledField.js
@@ -1,34 +1,36 @@
 import React from 'react';
 import { Field } from 'redux-form';
-import { Form } from 'semantic-ui-react';
+import { Form, Input } from 'semantic-ui-react';
 
-class ControlledField  extends React.Component {
+class ControlledField extends React.Component {
   innerComponent = innerProps => {
     const { input, meta, ...innerPropsRest } = innerProps;
     const actualComponentProps = {
       ...innerPropsRest,
       ...input,
+      onChange: (event, data) => input.onChange(data.value),
       error: meta.touched && meta.invalid,
       loading: meta.asyncValidating
     };
     return (
-      <div style={{ marginBottom: '14px' }}>
+      <Form.Field style={{ marginBottom: '14px' }}>
+        <label>{this.props.label}</label>
         {React.createElement(this.props.component, actualComponentProps)}
         {actualComponentProps.error &&
-          <div style={{ marginTop: '-10px', fontSize: '0.8rem', color: '#9f3a38' }}>{meta.error}</div>
+          <div style={{ fontSize: '0.8rem', color: '#9f3a38' }}>{meta.error}</div>
         }
-      </div>
+      </Form.Field>
     );
   };
 
   render() {
-    const { component, ...rest } = this.props;
+    const { component, label, ...rest } = this.props;
     return <Field component={this.innerComponent} {...rest} />;
   }
 }
 
 ControlledField.defaultProps = {
-  component: Form.Input
+  component: Input
 };
 
 export default ControlledField;
