9eff593afbe65f7444023d137116481d4b2e78e2
Jonathan Christopher <jonathan.christopher@outlook.com>
2018-01-06 15:34:37 +0700

    Create new app structure

---
+++ src/common/constants.js
@@ -0,0 +1,32 @@
+
+export const userStatuses = [
+  { key: 'active', value: 'active', text: 'Aktif' },
+  { key: 'awaiting_validation', value: 'awaiting_validation', text: 'Menunggu validasi' },
+  { key: 'disabled', value: 'disabled', text: 'Tidak aktif' }
+];
+
+export const genders = [
+  { key: 'male', value: 'male', text: 'Laki-laki' },
+  { key: 'female', value: 'female', text: 'Perempuan' }
+];
+
+export const departments = [
+  { key: 'FITB', value: 'FITB', text: 'Fakultas Ilmu Teknologi Kebumian' },
+  { key: 'FMIPA', value: 'FMIPA', text: 'Fakultas Matematika dan Ilmu Pengetahuan Alam' },
+  { key: 'FSRD', value: 'FSRD', text: 'Fakultas Seni Rupa dan Desain' },
+  { key: 'FTI', value: 'FTI', text: 'Fakultas Teknologi Industri' },
+  { key: 'FTMD', value: 'FTMD', text: 'Fakultas Teknik Mesin dan Dirgantara' },
+  { key: 'FTTM', value: 'FTTM', text: 'Fakultas Teknik Pertambangan dan Perminyakan' },
+  { key: 'FTSL', value: 'FTSL', text: 'Fakultas Teknik Sipil dan Lingkungan' },
+  { key: 'SAPPK', value: 'SAPPK', text: 'Sekolah Arsitektur, Perencanaan dan Pengembangan Kebijakan' },
+  { key: 'SBM', value: 'SBM', text: 'Sekolah Bisnis dan Manajemen' },
+  { key: 'SF', value: 'SF', text: 'Sekolah Farmasi' },
+  { key: 'SITH', value: 'SITH', text: 'Sekolah Ilmu dan Teknologi Hayati' },
+  { key: 'STEI', value: 'STEI', text: 'Sekolah Teknik Elektro dan Informatika' }
+];
+
+export const displayDateFormat = 'D MMMM YYYY';
+export const displayTimeFormat = 'HH:mm';
+export const displayDateTimeFormat = displayDateFormat + ' ' + displayTimeFormat;
+export const storeDateFormat = 'YYYY-MM-DD';
+export const storeDateTimeFormat = 'YYYY-MM-DDTHH:mm:ssZ';
